ec3aeb4f7d624b15bd51d289c8739194b37fb7a2
Alvaro Schmit <alvaroschmit@gmail.com>
2021-02-15 16:26:35 -0300

    control de error dio error jaja

--- js/coderflix.js
+++ js/coderflix.js
@@ -24,6 +24,11 @@ function cargoContenidoStreaming() {
             </div>`
             })
             $("#contenido").html(HTMLCard)
+        },
+        error: function() {
+            HTMLCard = `<h4>contenido no disponible :(</h4>`
+            $("#contenido").html(HTMLCard)
+
         }
     })
 
